41769cef3899b7230cb1ef3a5b660e4e1bd83ad1
Hristo Oskov <hristo@openlattice.com>
2021-11-03 17:52:24 -0700

    search reports neighbors using agency entity set ids

--- src/containers/report/ReportSagas.js
+++ src/containers/report/ReportSagas.js
@@ -5,20 +5,25 @@
 import {
   call,
   put,
+  select,
   takeEvery,
-  select
 } from '@redux-saga/core/effects';
 import { Map, fromJS } from 'immutable';
 import { SearchApi } from 'lattice';
 import type { SequenceAction } from 'redux-reqseq';
 
-import { getEntityKeyId } from '../../utils/DataUtils';
-import { getAppFromState, getEntitySetId, getUserIdFromState } from '../../utils/AppUtils';
-import { APP_TYPES } from '../../utils/constants/DataModelConstants';
+import { LOAD_REPORTS, loadReports } from './ReportActionFactory';
+
 import {
-  LOAD_REPORTS,
-  loadReports
-} from './ReportActionFactory';
+  getAppFromState,
+  getEntitySetId,
+  getSelectedOrganizationId,
+  getUserIdFromState,
+} from '../../utils/AppUtils';
+import { getEntityKeyId } from '../../utils/DataUtils';
+import { AGENCY_VEHICLE_RECORDS_ENTITY_SETS } from '../../utils/constants';
+import { APP_TYPES } from '../../utils/constants/DataModelConstants';
+import { APP } from '../../utils/constants/StateConstants';
 
 declare var __MAPBOX_TOKEN__;
 
@@ -49,22 +54,20 @@ function* loadReportsWorker(action :SequenceAction) :Generator<*, *, *> {
     const reportsForUser = reportNeighbors[userEntityKeyId];
 
     if (reportsForUser) {
-
       fromJS(reportsForUser).forEach((neighborObj) => {
         const neighborDetails = neighborObj.get('neighborDetails');
         const entityKeyId = getEntityKeyId(neighborDetails);
         reports = reports.set(entityKeyId, neighborDetails);
       });
 
-      readsByReport = yield call(
-        SearchApi.searchEntityNeighborsWithFilter,
-        reportsEntitySetId,
-        {
-          entityKeyIds: reports.keySeq().toJS(),
-          sourceEntityKeyIds: [readsEntitySetId],
-          destinationEntitySetIds: []
-        }
-      );
+      const orgId = getSelectedOrganizationId(app);
+      const appSettings = app.getIn([APP.SETTINGS_BY_ORG_ID, orgId]);
+      const agencyVehicleRecordsEntitySets = appSettings.get(AGENCY_VEHICLE_RECORDS_ENTITY_SETS) || Map();
+      readsByReport = yield call(SearchApi.searchEntityNeighborsWithFilter, reportsEntitySetId, {
+        entityKeyIds: reports.keySeq().toJS(),
+        sourceEntitySetIds: [readsEntitySetId, ...agencyVehicleRecordsEntitySets.keySeq().toJS()],
+        destinationEntitySetIds: []
+      });
 
       readsByReport = fromJS(readsByReport);
     }
